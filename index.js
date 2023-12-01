20faed3a874ce320b5584d7b02840a8bc207a6b8
JamesKevinson1901 <128903317+James-K-DNg-1901@users.noreply.github.com>
2023-12-01 16:53:26 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,31 +1,43 @@
-var email = document.getElementById("e-in");
-var submit = document.getElementById("sub-btn");
-var dismiss = document.getElementById("dismiss-btn");
-var main = document.getElementById("deal-register");
-var notify = document.getElementById("completed-notify");
+const nav_bar = document.getElementById("my-nav");
+const nav_element = document.getElementsByTagName("a");
+const open_btn = document.getElementById("open-btn");
+const close_btn = document. getElementById("close-btn");
+const faded_layer = document.getElementById("faded-layer");
 
-var filter = /^([a-zA-Z0-9_\.\-])+\@(([a-zA-Z0-9\-])+\.)+([a-zA-Z0-9]{2,4})+$/;
-
-submit.addEventListener("click", function complete() {
-    if (email.value == "") {
-        document.getElementById("warning").style.visibility = "visible";
-        email.classList.add("error");
-    } else {
-        if (email.value.match(filter)) {
-            document.getElementById("warning").style.visibility = "hidden";
-            email.classList.remove("error");
-            document.getElementById("show-e").innerText = email.value;
-            main.hidden = true;
-            notify.hidden = false;
-        } else {
-            document.getElementById("warning").style.visibility = "visible";
-            email.classList.add("error");
+window.onresize = function change_lauch(pageWidth)
+{
+    var pageWidth  = document.documentElement.scrollWidth;
+    if (pageWidth <= 900) {
+        nav_bar.classList.remove("selection-box");
+        nav_bar.classList.add("hidden-selection-box");
+        nav_bar.classList.add("hide_nav");
+        open_btn.hidden = false;
+        for (var i = 0; i < 5; i++) {
+            nav_element[i].classList.add("hidden-selection-element");
+            nav_element[i].classList.remove("selection-element");
         }
-    }
-});
+        open_btn.addEventListener ("click", function open_nav () {
+            nav_bar.classList.add("show_nav");
+            nav_bar.classList.remove("hide_nav");
+            close_btn.hidden = false;
+            faded_layer.hidden = false;
+        });
+        close_btn.addEventListener("click", function close_nav () {
+            nav_bar.classList.remove("show_nav");
+            nav_bar.classList.add("hide_nav");
+            close_btn.hidden = true;
+            faded_layer.hidden = true;
+        })
+        
+    } else {
+        nav_bar.classList.add("selection-box");
+        nav_bar.classList.remove("hidden-selection-box");
+        nav_bar.classList.remove("hide_nav");
+        open_btn.hidden = true;
+        for (var i = 0; i < 5; i++) {
+            nav_element[i].classList.add("selection-element");
+            nav_element[i].classList.remove("hidden-selection-element");
+        }   
+    } 
+};
 
-dismiss.addEventListener("click", function dismiss() {
-    main.hidden = false;
-    notify.hidden = true;
-    email.value = "";
-});
